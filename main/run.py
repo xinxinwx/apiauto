9411c6ac2e0241bd8269e4bca3ccab68658bf5dd
鑫鑫 <380222985@qq.com>
2020-11-02 16:34:40 +0800

    报告

--- main/run.py
+++ main/run.py
@@ -19,6 +19,6 @@ if __name__ == '__main__':
     # 要执行的测试用例
     case_path = 'test_api.py'
     xml_report_path = 'html'
-    args = ['-s', '-q', case_path, '--html=html/bg.html']
-    # args = ['-s', '-q', case_path, '--alluredir=./html/report']
+    # args = ['-s', '-q', case_path, '--html=html/bg.html']
+    args = ['-s', '-q', case_path, '--alluredir=./html/report']
     pytest.main(args=args)
